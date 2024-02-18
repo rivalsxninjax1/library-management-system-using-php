6e3d6dbbb27926d6a4bb02c036f838bd1f8c94c4
Sabin sharma <nunjax69@Sabins-MacBook-Pro.local>
2024-02-18 08:24:56 +0545

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy;Online Library Management System |<a href="https://facebook.com/simrikasubedi" target="_blank"  > Designed by : Simrika Subedi</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
